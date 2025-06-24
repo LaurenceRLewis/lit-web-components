5e46103cb60b9ca34b57e1fa0f5b9c4a7c93784f
Laurence Lewis <laurence.r.lewis@gmail.com>
2025-06-24 14:04:05 +1000

    basic input

---
+++ src/components/input/input.stories.ts
@@ -0,0 +1,24 @@
+import './input.ts';
+
+const Template = ({ label }: { label: string }) =>
+  `<form-input label="${label}"></form-input>`;
+
+export default {
+  title: 'Components/Input',
+  tags: ['autodocs'],
+  component: 'form-input',
+  argTypes: {
+    label: {
+      control: { type: 'select' },
+      options: ['Given name', 'Family name', 'Email', 'Mobile'],
+    },
+  },
+};
+
+// CSF3 story using `args`
+export const Default = {
+  render: Template,
+  args: {
+    label: 'Given name',
+  },
+};
